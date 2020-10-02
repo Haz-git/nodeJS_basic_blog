49abfc381b8bd35c29ba753e0e9658b2afa82369
Haz-git <zhouharry1@gmail.com>
2020-10-02 08:59:24 -0700

    Added Routing

--- index.js
+++ index.js
@@ -4,15 +4,42 @@ const fs = require('fs');
 const url = require('url');
 
 //Extracting data from HTML files synchronously to prevent constant re-rendering of files:
-fs.readFileSync(`${__dirname}/templates/landing.html`,'utf-8');
+const landingPage = fs.readFileSync(`${__dirname}/templates/landing.html`,'utf-8');
+const blogPage = fs.readFileSync(`${__dirname}/templates/blog.html`,'utf-8');
+const blogList = fs.readFileSync(`${__dirname}/templates/blogList.html`,'utf-8');
 
 //Creating the Server
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-type': 'text/html'
-    });
-    res.end('Hello World!');
+
+    const { pathname, query } = url.parse(req.url, true);
+    //I think the purpose of 'true' is to look for queries?
+
+    if (pathname === '/') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        res.end(landingPage);
+    } else if (pathname === '/bloglist') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+
+        res.end(blogList);
+    } else if (pathname === '/blog') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        
+        res.end(blogPage);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+
+        res.end('<h1>404 Not Found!</h1>');
+    }
 });
 
 
